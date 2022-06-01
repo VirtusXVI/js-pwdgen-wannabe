dccdf54155061a26babdd7be4f93bd50e9d5f76a
VirtusXVI <manuel.aveta2001@gmail.com>
2022-06-01 11:58:39 +0200

    upgraded complex password generation

--- js/script.js
+++ js/script.js
@@ -12,23 +12,28 @@ if(userChoice == 1){
     document.getElementById("password").innerHTML = userPassword;
 }
 // COMPLEX PASSWORD GENERATOR
-// VAR DECLARATION
 
 else if(userChoice == 2){
-    let userComplexPassword = String(parseInt(Math.random(10)*10));
+    // VAR DECLARATION
     var letters =   ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
     ,"r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q"
     ,"R","S","T","U","V","W","X","Y","Z"];
-    let i = 0;
-    for (i = 0; i < 16; i++){
+    // GENERATING THE FIRST CHAR OF THE PASSWORD
+    let userLength = prompt("inserisci la lunghezza della password")
+    let userFirstChar = parseInt(Math.random()*52);
+    let userComplexPassword = letters[userFirstChar];
+    // GENERATING THE REST OF THE PASSWORD
+    for (let i = 0; i < (userLength - 1); i++){
         let userRandomnumber = Math.random()
         let userNumber = parseInt(userRandomnumber * 52);
         userChar = letters[userNumber];
         userComplexPassword = userComplexPassword + userChar;
     }
+    // PRINTING THE PASSWORD
     document.getElementById("password").innerHTML = "la tua password è: " + userComplexPassword;
     }
 else{
+    // userChoice IS NOT VALID
     alert("il numero inserito non è valido")
     location.reload()
 }
